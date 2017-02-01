b6625057107046b76848864daafe40ea29795855
Aditya Dwivedi <ad629@cornell.edu>
2017-02-01 18:26:03 -0500

    removed redundant code

--- MNazar/AppDelegate.swift
+++ MNazar/AppDelegate.swift
@@ -46,14 +46,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        if loggedIn {
-            locationManager.stopUpdatingLocation()
-            locationManager.startUpdatingLocation()
-        }
-    }
+//    func applicationWillResignActive(_ application: UIApplication) {
+//        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+//        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+//        if loggedIn {
+//            locationManager.stopUpdatingLocation()
+//            locationManager.startUpdatingLocation()
+//        }
+//    }
     
     /*
      * Gets difference between two Date objects in hours
